73b9523ae6a8e0518d3976234d9f5f7e718a9403
Shivam Prajapati <shivamsprajapati13@gmail.com>
2025-05-25 19:41:21 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/patient-register-app",
   optimizeDeps: {
     
     exclude: ['@electric-sql/pglite']
